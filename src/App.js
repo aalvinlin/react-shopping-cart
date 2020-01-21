24ef11b42f129d2b34551417a0e73d6e4f18006d
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-01-21 13:20:18 -0800

    ProductContext created. Step 3 finished

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+import ProductContext from "./contexts/ProductContext";
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import Product from './components/Product';
 
 function App() {
 	const [products] = useState(data);
@@ -22,21 +25,20 @@ function App() {
 			<Navigation cart={cart} />
 
 			{/* Routes */}
+			<ProductContext.Provider value={{products, addItem}}>
 			<Route
 				exact
 				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
+				component={Products}
 			/>
+			</ProductContext.Provider>
 
+			<ProductContext.Provider value={{products, addItem}}>
 			<Route
 				path="/cart"
 				render={() => <ShoppingCart cart={cart} />}
 			/>
+			</ProductContext>
 		</div>
 	);
 }
